82890835e61f534e17d4f9d1e128653727f73928
Luiz Bartolassi <luizekbartolassi@gmail.com>
2022-11-27 12:09:24 -0300

    creating backend logic with flask

--- app.py
+++ app.py
@@ -1,45 +1,56 @@
 from model.curso import Curso
 from model.estudante import Estudante
+from dao.DAO import DAO
 from dao.cursoDAO import CursoDAO
 
+# set FLASK_APP=app.py && flask run
+from flask import Flask, render_template as render, request
 
-cursoDAO = CursoDAO()
-
-adm = Curso(1, "Administração", "ADM")
-ea = Curso(2, "Engenharia de Alimentos", "EA")
-eca = Curso(3, "Engenharia de Controle e Automação", "ECA")
-ec = Curso(4, "Engenharia de Computação", "EC")
-
-cursoDAO.inserir(adm)
-cursoDAO.inserir(ea)
-cursoDAO.inserir(eca)
-cursoDAO.inserir(ec)
+app = Flask(__name__)
 
 
-for curso in cursoDAO.listar():
-    print(curso.get('nome'), curso.get('sigla'))
+@app.route('/')
+def runing():
+    return "<h5>running</h5>"
 
 
-print(adm.getCodigo(), adm.getNome(), adm.getSigla())
-print(ea.getCodigo(), ea.getNome(), adm.getSigla())
-print(eca.getCodigo(), eca.getNome(), eca.getSigla())
-print(ec.getCodigo(), ec.getNome(), ec.getSigla())
+@app.route('/app-school')
+def index():
+    return render('index.html')
 
 
-alunoA = Estudante(1, "Luiza", "081220001", ea)
-alunoB = Estudante(2, "Luiz", "081220002", eca)
+#######################################
+#                                     #
+# .:Curso                             #
+#                                     #
+#######################################
 
-print()
-print("Estudantes:")
-print("\tCodigo: ", alunoA.getCodigo())
-print("\tMatricula: ", alunoA.getMatricula())
-print("\tNome: ", alunoA.getNome())
-print("\tCurso: ", alunoA.getCurso().getCodigo(),
-      '-', alunoA.getCurso().getNome())
+@app.route('/app-school/curso/listar')
+def curso_listar():
+    return render('/curso/listar.html')
 
-print()
-print("\tCodigo: ", alunoB.getCodigo())
-print("\tMatricula: ", alunoB.getMatricula())
-print("\tNome: ", alunoB.getNome())
-print("\tCurso: ", alunoB.getCurso().getCodigo(),
-      '-', alunoB.getCurso().getNome())
+
+@app.route('/app-school/curso/novo')
+def curso_novo():
+    return render('/curso/inserir.html')
+
+
+@app.route('/app-school/curso/inserir', methods=['GET', 'POST'])
+def curso_inserir():
+    nome = request.form['nome']
+    sigla = request.form['sigla']
+
+    curso = Curso(1, nome, sigla)
+    CursoDAO().inserir(curso)
+    return "deu bom"
+
+
+#######################################
+#                                     #
+# .:Estudante                         #
+#                                     #
+#######################################
+
+@app.route('/app-school/estudante/listar')
+def estudante_listar():
+    return render('/estudante/listar.html')
